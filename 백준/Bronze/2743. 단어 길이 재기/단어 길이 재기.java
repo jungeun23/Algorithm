dd9d980c942428d2e864299b6626602d1ce352fa
jungeun23 <80149459+jungeun23@users.noreply.github.com>
2024-02-01 20:41:35 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 200 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    private static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        String result = "";
+        String line = sc.nextLine();
+
+
+
+        System.out.print(line.length());
+
+
+    }
+}
